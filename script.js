57e9b4766ed75acc02c267e0fcb95f1f74d0b6a7
peter-zay <peter.zay@gmail.com>
2024-03-01 15:11:37 -0700

    changed projects to dropdown list

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const darkModeToggle = document.getElementById('dark-mode-toggle');
-    const body = document.body;
+    const dropdown = document.getElementById('projects-dropdown');
 
-    darkModeToggle.addEventListener('click', function() {
-        body.classList.toggle('dark-mode');
-    });
+    if (dropdown) {
+        dropdown.addEventListener('change', function() {
+            const selectedOption = dropdown.options[dropdown.selectedIndex];
+            console.log('Selected Option:', selectedOption);
+
+            if (selectedOption && selectedOption.value) {
+                const projectUrl = selectedOption.value;
+                console.log('Project URL:', projectUrl);
+                window.location.href = projectUrl;
+            } else {
+                console.error('Selected option or value is missing.');
+            }
+        });
+    } else {
+        console.error('Dropdown element not found.');
+    }
 });
